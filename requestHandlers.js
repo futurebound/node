81369ef196b02bb110ed90804cde0f3348dd06b1
Mitchell M <mitchellm1618@gmail.com>
2018-01-14 21:04:20 -0800

    refactored pages and added tmp folder for img upload

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var querystring = require('querystring');
+    fs = require('fs';)
 
 function start(res, postData) {
   console.log(`Request handler 'start' was called.`);
@@ -29,5 +30,12 @@ function upload(res, postData) {
   res.end();
 }
 
+function show(res) {
+    console.log("Request handler 'show' was called.");
+    res.writeHead(200, {"Content-Type": "image/png"});
+    fs.createReadStream("/tmp/test.png").pipe(response);
+}
+
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
