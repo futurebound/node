384d17d9a9176301eb55ea61ce365991223974d1
Mitchell M <mitchellm1618@gmail.com>
2018-01-14 20:07:02 -0800

    refactored server.js and added index.js

--- server.js
+++ server.js
@@ -2,8 +2,15 @@
 
 var http = require('http');
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
+function start() {
+  function onRequest(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
     res.write('Hello World!');
     res.end();
-}).listen(8888);
+  }
+
+  http.createServer(onRequest).listen(8888);
+  console.log('Server has started.');
+}
+
+exports.start = start;
