ced8d8bf08df3866c67283f433417e27cf75bb04
Mitchell M <mitchellm1618@gmail.com>
2018-01-14 20:27:51 -0800

    refactored files improperly

--- router.js
+++ router.js
@@ -3,9 +3,10 @@
 function route(pathname) {
   console.log('About to route a request for ' + pathname);
   if (typeof handle[pathname] === 'function') {
-    handle[pathname]();
+    return handle[pathname]();
   } else {
     console.log('No request handler found for ' + pathname);
+    return '404 Not Found';
   }
 }
 
