1164a908786d4747f8a6ef0803a496ecf47924b8
Mitchell M <mitchellm1618@gmail.com>
2018-01-14 20:10:52 -0800

    added router.js and added its module to server.js

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 'use strict';
 
 var http = require('http');
+var url = require('url');
 
 function start() {
   function onRequest(req, res) {
+    var pathname = url.parse(req.url).pathname;
+    console.log('Request for ' + pathname + 'received.');
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.write('Hello World!');
     res.end();
