298fc739fe6b4433b8f21f30de142722710eff95
Mitchell M <mitchellm1618@gmail.com>
2018-01-14 20:51:39 -0800

    refactored more files

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,21 +1,31 @@
 'use strict';
 
-var exec = require('child_process').exec;
+var querystring = require('querystring');
 
-function start() {
-  console.log('Request handler \'start\' was called.');
+function start(res, postData) {
+  console.log(`Request handler 'start' was called.`);
 
-  exec('ls -lah', function (error, stdout, stderr) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write(stdout);
-    res.end();
-  });
+  var body = '<html>' +
+    '<head>' +
+    '<meta http-equiv="Content-Type" content="text/html"; ' +
+    '</head>' +
+    '<body>' +
+    '<form action="/upload" method="post">' +
+    '<textarea name="text" rows="20" cols="60"></textarea>' +
+    '<input type="submit" value="Submit text" />' +
+    '</form>' +
+    '</body>' +
+    '</html>';
+
+  res.writeHead(200, {"Content-Type": "text/html"});
+  res.write(body);
+  res.end();
 }
 
-function upload() {
-  console.log('Request handler \'upload\' was called.');
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.write('Hello Upload');
+function upload(res, postData) {
+  console.log(`Request handler 'upload' was called.`);
+  res.writeHead(200, {"Content-Type": "text/plain"});
+  res.write("You've sent the text: " + querystring.parse(postData).text);
   res.end();
 }
 
